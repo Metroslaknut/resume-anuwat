4cd0e31e1a2b0ac8fc3e33216da2eb2d94afd73d
anuwat <metroslak.nut.en@gmail.com>
2026-03-02 22:43:16 +0700

    Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -27,31 +27,22 @@ const addressData = [
 
 const Contact = () => {
   return (
-    <div className="relative -bottom-15 -mt-15 z-10 px-2">
+    <div className="relative -bottom-15 -mt-15 z-10 px-2 flex justify-center w-full">
       <div
-        className="content p-4 md:p-10 lg:p-22 bg-white rounded-2xl shadow-[0px_0px_90px_9px_rgba(0,_0,_0,_0.1)]"
+        className="content max-w-[1200px] w-full p-4 md:p-10 lg:p-22 bg-white rounded-2xl shadow-[0px_0px_90px_9px_rgba(0,_0,_0,_0.1)] mx-auto"
         id="contact"
       >
-        <div className="flex flex-col-reverse lg:gap-5 xl:gap-25.75 lg:flex-row justify-between">
-          <div>
-            <div>
-              <p className="text-[35px] max-lg:hidden font-semibold text-nowrap text-[#132238]">
-                ช่องทางการติดต่อ
-              </p>
-              <p className="text-[12px] xs:text-[14px] sm:text-lg md:text-lg max-lg:text-center pt-4 font-normal text-soft-dark">
-                สามารถติดต่อผมได้ผ่านช่องทางต่างๆ ดังนี้ หากต้องการพูดคุยเกี่ยวกับการสมัครงาน
-              </p>
-            </div>
-            <div className="my-8.75 flex flex-row flex-nowrap gap-8 justify-center items-stretch overflow-x-auto px-4" style={{minHeight: '140px'}}>
-              {addressData.map((item, index) => (
-                <Address item={item} key={index} />
-              ))}
-            </div>
-          </div>
-          <div className="w-full overflow-y-scroll py-6.5">
-            <p className="text-xl mb-2 xs:text-2xl sm:text-2xl md:text-[38px] font-semibold text-[#132238] lg:hidden text-center">
-              ช่องทางการติดต่อ
-            </p>
+        <div className="flex flex-col items-center w-full">
+          <p className="text-[35px] font-semibold text-nowrap text-[#132238] text-center mb-2">
+            ช่องทางการติดต่อ
+          </p>
+          <p className="text-[12px] xs:text-[14px] sm:text-lg md:text-lg text-center pt-4 font-normal text-soft-dark mb-4">
+            สามารถติดต่อผมได้ผ่านช่องทางต่างๆ ดังนี้ หากต้องการพูดคุยเกี่ยวกับการสมัครงาน
+          </p>
+          <div className="my-8.75 flex flex-row flex-nowrap gap-8 justify-center items-stretch overflow-x-auto px-4 w-full">
+            {addressData.map((item, index) => (
+              <Address item={item} key={index} />
+            ))}
           </div>
         </div>
       </div>
